220e2c97fcf68b2893905019f51ed8b83172148d
skammm <596435525@qq.com>
2020-01-22 20:42:02 +0800

    7

--- router.js
+++ router.js
@@ -3,6 +3,7 @@
 var express = require('express');
 
 var router = express.Router();
+var md5 = require('blueimp-md5');
 
 var User = require('./models/user');
 
@@ -34,6 +35,7 @@ router.post('/register',function(req,res){
         message:'email or name is exits'
       });
     }
+    body.password = md5(md5(body.password));
     new User(body).save(function(err,user){
       if(err){
         return res.status(500).json({
